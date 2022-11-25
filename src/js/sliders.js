8dbd3a793be1bf3349ba5d8b36ad5cc70eb894fe
d.parshakov <d.parshakov@platiza.ru>
2022-11-25 18:26:40 +0500

    autoplay reviews slider

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -4,22 +4,21 @@ import Swiper from 'swiper/bundle';
 // import styles bundle
 import 'swiper/css/bundle';
 $(document).ready(function() {
-    const swiper = new Swiper(".mySwiper", {
+    const swiper = new Swiper(".reviewsSwiper", {
+        loop: true,
         autoHeight: true,
         spaceBetween: 20,
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
         },
     });
-
 });
-
-// autoplay: {
-//     delay: 2500,
-//         disableOnInteraction: false,
-// },
